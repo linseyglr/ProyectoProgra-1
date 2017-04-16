80f9fed1fda6edda541c5bed7bf7442702916d30
linseyglr <Lindsay9@hotmail.es>
2017-04-16 17:15:17 -0500

    Add files via upload

---
+++ Proyecto/src/proyecto/Proyecto.java
@@ -0,0 +1,17 @@
+
+package proyecto;
+
+/**
+ *Descripcion de la clase: Main
+ *Fecha de creacion: 16 de abril 2017
+ *Autores: Linsey Garro Le Roy, Ramson Gu Wu
+ *Revisor: Georges Alfaro Salazar
+ */
+public class Proyecto {
+
+   
+    public static void main(String[] args) {
+        
+    }
+    
+}
