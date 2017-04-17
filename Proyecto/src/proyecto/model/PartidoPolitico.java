f5107906396a959909f5aa03761ca73bb3bee252
linseyglr <Lindsay9@hotmail.es>
2017-04-16 18:30:25 -0600

    Se crea la clase Partido Politico División de paquetes

---
+++ Proyecto/src/proyecto/model/PartidoPolitico.java
@@ -0,0 +1,14 @@
+
+package proyecto.model;
+
+/**
+ *Descripcion de la clase: Clase utilizada para la declaración de la constitución de un partido político
+ *Fecha de creacion: 16 de abril 2017
+ *Autores: Linsey Garro Le Roy, Ramson Gu Wu
+ *Revisor: Georges Alfaro Salazar
+ */
+public class PartidoPolitico {
+    
+    int cantidadDeVotos;
+    
+}
