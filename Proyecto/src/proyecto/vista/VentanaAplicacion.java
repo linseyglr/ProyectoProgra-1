6a5a148c8800613bb6fcd339cc75a420be18923a
linseyglr <Lindsay9@hotmail.es>
2017-04-16 18:52:09 -0600

    Implementacion de lo basico

---
+++ Proyecto/src/proyecto/vista/VentanaAplicacion.java
@@ -0,0 +1,29 @@
+ 
+package proyecto.vista;
+
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JFrame;
+
+/**
+ *Descripcion de la clase: Modelo
+ *Fecha de creacion: 16 de abril 2017
+ *Autores: Linsey Garro Le Roy, Ramson Gu Wu
+ *Revisor: Georges Alfaro Salazar
+ */
+
+ 
+public class VentanaAplicacion extends JFrame
+        implements Observer {
+
+   
+    
+    @Override
+    public void update(Observable o, Object o1) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+    
+    
+    
+}
